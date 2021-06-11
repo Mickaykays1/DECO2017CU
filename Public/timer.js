d63869498ee2137427950421ca8d643732d2ef72
Mickaykays1 <mcfranz98@gmail.com>
2021-06-12 00:00:39 +1000

    does this still count?

--- Public/timer.js
+++ Public/timer.js
@@ -1,17 +1,7 @@
-const TIME_LIMIT = 1200;
-let timePassed = 0;
-let timeLeft = TIME_LIMIT;
+
+
+
 let timerInterval = null;
-
-
-document.getElementById("clock").innerHTML = `
-    <div>
-        <div id="label">
-        </div>
-        <button onclick= "countdown()" type= "button" class="btn btn-light">Start</button>
-        <button onclick= "myStopFunction()" type= "button" class="btn btn-light">Pause</button>
-    </div>
-`
 
 function formatTime(time) {
     let minutes = Math.floor(time/60)
@@ -25,24 +15,28 @@ function formatTime(time) {
     return `${minutes}: ${seconds}`
 }
 
-
-
-
 function countdown() {
-        timerInterval = setInterval(()=> {
-            timePassed += 1;
-            timeLeft= TIME_LIMIT - timePassed;
-            document.getElementById("label").innerHTML =
-                formatTime(timeLeft);
-            if (timeLeft == 0) {
-                myStopFunction()
-            }
-            
-        },1000);
+    let timerInput = document.getElementById('timer')
+    let TIME_LIMIT = timerInput.value;
+    timerInterval = setInterval(()=> {
+        let timeLeft= TIME_LIMIT -= 1;
+        document.getElementById("label").innerHTML =
+            formatTime(timeLeft);
+        if (timeLeft == 0) {
+            myStopFunction()
+        }
+    },1000);
  
 }
 
 function myStopFunction() {
     clearInterval(timerInterval);
-       
 }
+document.getElementById("clock").innerHTML = `
+    <div>
+        <div id="label">
+        </div>
+        <button onclick= "countdown()" type= "button" class="btn btn-light">Start</button>
+        <button onclick= "myStopFunction()" type= "button" class="btn btn-light">Pause</button>
+    </div>
+`
